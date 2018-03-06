6b429700d8c7f671d6ab4e558114cf9813e632ed
Raghu Rudraraju <Raghu.Rudraraju@copart.com>
2018-03-06 16:25:26 +0530

    Added reducers and stores.

--- src/App.js
+++ src/App.js
@@ -1,11 +1,26 @@
 import React, { Component } from 'react';
 import './App.css';
+import { createStore } from 'redux';
 
-class App extends Component {
+function rootReducer(state = {}, action) {
+  switch (action.type) {
+    case 'ADD-ONE':
+    return state.concat('added one')
+    case 'SUBTRACT-ONE':
+    return state.concat('subtract one')
+    default:
+    return store;
+  }
+}
+
+let store = createStore(rootReducer);
+
   render() {
     return (
       <div className="App">
           <h1>Simple React Redux Counter Application</h1>
+          <button>Increment</button>
+          <button>Decrement</button>
       </div>
     );
   }
