59c7d49ca122cdb48ed4072e3c16a10463e0eeb7
Raghu Rudraraju <Raghu.Rudraraju@copart.com>
2018-03-08 16:02:22 +0530

    Updated code with modified increment and decrement fucntionalities.

--- src/App.js
+++ src/App.js
@@ -2,41 +2,48 @@ import React, { Component } from 'react';
 import './App.css';
 import { createStore } from 'redux';
 
-function rootReducer(state = [], action) {
+function rootReducer(state = {value:0}, action) {
   switch (action.type) {
     case 'ADD-ONE':
-    return state.concat(action.data)
+    return {value: state.value + 1}
     case 'SUBTRACT-ONE':
-    return state.concat(action.data)
+    return {value: state.value -= 1}
     default:
-    return store;
+    return state;
   }
 }
 
 let store = createStore(rootReducer);
 
 class App extends Component {
+
   constructor() {
     super()
     store.subscribe(() => {
       console.log(store.getState());
     });
+    
+    this.state = {
+      value: 0,
+    }
+
   }
 
   increment(){
     console.log('Increment Method. ')
-    store.dispatch({type: 'ADD-ONE', data: 'Add one'});
+    store.dispatch({type: 'ADD-ONE'});
   }
 
   decrement(){
     console.log('Increment Method. ')
-    store.dispatch({type: 'SUBTRACT-ONE', data: 'Remove one'});
+    store.dispatch({type: 'SUBTRACT-ONE'});
   }
 
   render() {
     return (
       <div className="App">
           <h1>Simple React Redux Counter Application</h1>
+          <div>State Value: {this.state.value} </div>
           <button onClick={this.increment}>Increment</button>
           <button onClick={this.decrement}>Decrement</button>
       </div>
